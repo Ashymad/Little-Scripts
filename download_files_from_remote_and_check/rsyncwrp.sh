848127d84de2e4c4c1c70df5d59bf761caf00a4e
Ashymad <czilukim@o2.pl>
2017-10-02 00:32:34 +0200

    Allow different remote and local dirs

--- download_files_from_remote_and_check/rsyncwrp.sh
+++ download_files_from_remote_and_check/rsyncwrp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Download files from server
-rsync -vrzPL --append-verify $1:$2/ $2
+rsync -vrzPL --append-verify $1:$2/ $3
 # Check if hash matches, then delete if it does
 ssh $1 find $2/ -type l -print0 | xargs -0 -I {} sh -c "ssh $1 \"md5sum \\\"{}\\\"\" | md5sum --status -c - && ssh $1 \"rm \\\"{}\\\"\";"
 # Remove any empty directories
