95671a1738d6d7933b49a6f91735e09886837b28
Ashymad <czilukim@o2.pl>
2017-10-01 08:58:20 +0200

    added rsyncwrp

---
+++ download_files_from_remote_and_check/rsyncwrp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Download files from server
+rsync -vrzPL --append-verify $1:$2/ $2
+# Check if hash matches, then delete if it does
+ssh $1 find $2/ -type l -print0 | xargs -0 -I {} sh -c "ssh $1 \"md5sum \\\"{}\\\"\" | md5sum --status -c - && ssh $1 \"rm \\\"{}\\\"\";"
+# Remove any empty directories
+ssh $1 find $2/ -mindepth 1 -type d -empty -delete
+
