49120a060defbcdb8222d21a09dc126ba89a88a2
Ashymad <czilukim@o2.pl>
2017-10-02 11:33:20 +0200

    Fix bugs

--- download_files_from_remote_and_check/rsyncwrp.sh
+++ download_files_from_remote_and_check/rsyncwrp.sh
@@ -3,7 +3,8 @@
 # Download files from server
 rsync -vrzPL --append-verify $1:$2/ $3
 # Check if hash matches, then delete if it does
-ssh $1 find $2/ -type l -print0 | xargs -0 -I {} sh -c "ssh $1 \"md5sum \\\"{}\\\"\" | md5sum --status -c - && ssh $1 \"rm \\\"{}\\\"\";"
+cd $3
+ssh $1 "{ cd $2; find . -type l -print0; }" | xargs -0 -I{} sh -c "ssh $1 \"{ cd $2; md5sum \\\"{}\\\"; }\" | md5sum -c - && ssh $1 \"{ cd $2; rm -v \\\"{}\\\"; }\";"
 # Remove any empty directories
 ssh $1 find $2/ -mindepth 1 -type d -empty -delete
 
